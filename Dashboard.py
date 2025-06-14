956a257bf2fae45b918ce3962ee4aab4da524082
Rudy <medeiros.rudy@gmail.com>
2025-06-14 15:27:22 -0300

    Mensagem do commit (ajuste conforme a necessidade)

--- Dashboard.py
+++ Dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 
 # ----------Page Configs
